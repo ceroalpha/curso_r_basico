cbed7184219118cf3093dee4c27a29ee9d965692
ceroalpha <softdvn@hotmail.com>
2020-06-13 16:41:38 -0500

    Como Instalar paquetes y cargar librerias

--- scripts/tema1/03-install.R
+++ scripts/tema1/03-install.R
@@ -1,11 +1,16 @@
+#Instrucccion parainstalar un paquete , DEP=  significa que tambien instale las dependencias.
 install.packages("tidyverse", dep = TRUE)
 
-library(tidyverse)
+# Cargar las librerias asociadas al paquete tiyversey (para su uso)
+library(tidyverse) 
 
+#Paquete para crear cuadrados magicos
 install.packages("magic", dep = TRUE)
 
 library(magic)
-
+#Ejemplo de como pintar un ejemplo de cuadro magico (marriz de 6)
 magic(6)
 
+#Instruccion para saber los paquetes que estan instalados en R
 installed.packages()
+
